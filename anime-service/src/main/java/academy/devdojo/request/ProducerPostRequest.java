91120f1d7e5852a840fb5619ee83e4e16607f728
AVogelsanger <voguednj@hotmail.com>
2025-03-13 20:12:17 -0300

    video25: Exercise 06 PutMapping

--- anime-service/src/main/java/academy/devdojo/request/ProducerPostRequest.java
+++ anime-service/src/main/java/academy/devdojo/request/ProducerPostRequest.java
@@ -2,9 +2,11 @@ package academy.devdojo.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class ProducerPostRequest {
 
     private String name;
--- anime-service/src/main/java/academy/devdojo/request/ProducerPutRequest.java
+++ anime-service/src/main/java/academy/devdojo/request/ProducerPutRequest.java
@@ -3,13 +3,13 @@ package academy.devdojo.request;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Builder
-@ToString
-public class AnimePutRequest {
+public class ProducerPutRequest {
 
     private Long id;
     private String name;
